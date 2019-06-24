46dd75ba20e05e091ee9f34b7a59a0877aed678b
TrezRayanCo <43716183+TrezRayanCo@users.noreply.github.com>
2019-06-24 10:35:05 +0430

    Add files via upload

---
+++ Raygansms.framework/Headers/Raygansms.h
@@ -0,0 +1,19 @@
+//
+//  Raygansms.h
+//  Raygansms
+//
+//  Created by Ebrahim on 2019-05-09.
+//  Copyright © 2019 Ahamrah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Raygansms.
+FOUNDATION_EXPORT double RaygansmsVersionNumber;
+
+//! Project version string for Raygansms.
+FOUNDATION_EXPORT const unsigned char RaygansmsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Raygansms/PublicHeader.h>
+
+
